d6699a8c49664d080529e76580f3c214422d380f
Samuel Jesuthas <samueljesuthas27@gmail.com>
2023-04-28 19:42:49 +0100

    add iris dataset

--- kmeans.py
+++ kmeans.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 X, y = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
 
 # Apply K-Means algorithm
-kmeans = KMeans(n_clusters=4)
+kmeans = KMeans(n_clusters=4) #We are using the built-int library to execute the algorithm to save time
 kmeans.fit(X)
 
 # Get cluster centers and labels
@@ -16,4 +16,6 @@ labels = kmeans.labels_
 # Plot the data points and cluster centers
 plt.scatter(X[:,0], X[:,1], c=labels)
 plt.scatter(centers[:,0], centers[:,1], marker='x', s=200, linewidths=3, color='r')
+
+# Finally show the final graph
 plt.show()
